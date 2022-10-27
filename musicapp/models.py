bddac135d5fff4ca97faf8491ec7dc883b25a43a
ruona <ofohogheneruona@gmail.com>
2022-10-27 04:43:11 -0700

    initial changes made by Ruona

--- musicapp/models.py
+++ musicapp/models.py
@@ -13,7 +13,7 @@ class Song(models.Model):
     Artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     date_released = models.DateField(default=datetime.today)
-    artist_id = models.IntegerField()
+    #artist_id = models.IntegerField()
     likes = models.IntegerField()
 
     def __str__(self):
@@ -23,7 +23,7 @@ class Song(models.Model):
 class Lyric(models.Model):
     Song = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
-    song_id = models.IntegerField()
+    #song_id = models.IntegerField()
 
     def __str__(self):
         return self.content
