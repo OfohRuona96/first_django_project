e3b0b3468d43852fdc56254498e06d8ca177bc06
ruona <ofohogheneruona@gmail.com>
2022-10-27 02:38:18 -0700

    commit made by Ruona

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import index
+from .views import index
 
 app_name="musicapp"
 urlpatterns = [
-    path('', views.index, name="index"),
+    path("", index, name="index"),
 ]  
 
