da3c39162b3d72d57e9159f88b95a58f10072bde
ruona <ofohogheneruona@gmail.com>
2022-10-28 13:34:02 -0700

    changes made by Ruona

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,19 +1,17 @@
-from email.policy import default
+
 from django.db import models
 from datetime import datetime
 
 # Create your models here.
-
 class Artiste(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     age = models.IntegerField()
-
     def __str__(self):
-        return self.first_name 
+         return self.first_name 
 
 class Song(models.Model):
-    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    artiste_id = models.ForeignKey(Artiste, null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=40)
     date_released = models.DateField(default=datetime.today)
     likes = models.IntegerField()
@@ -24,11 +22,11 @@ class Song(models.Model):
 
 
 class Lyric(models.Model):
-   song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
-   content = models.CharField(max_length=1000)
+    song_id = models.ForeignKey(Song, null=True, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
    
 
-   def __str__(self):
+def __str__(self):
        return self.content
 
     
