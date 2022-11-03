545eb0a5486353e094f6777bdb037c03a5595442
ruona <ofohogheneruona@gmail.com>
2022-11-03 08:03:30 -0700

    commit made by ruona

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,6 +1,1 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello welcome to my App")
+from rest_framework import viewsets
