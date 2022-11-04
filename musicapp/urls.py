c25a93c28f4b12ab064136e5bc3e50448260a0ac
ruona <ofohogheneruona@gmail.com>
2022-11-04 12:24:55 -0700

    commit made by me

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -4,7 +4,7 @@ from .views import SongList, SongDetail, ArtisteList, ArtisteDetail
 
 urlpatterns = [
     path("song/<int:pk>/", SongDetail.as_view(), name="song_detail"),
-    path("", SongList.as_view(), name="song_list"),
+    path("song/", SongList.as_view(), name="song_list"),
     path("artiste/<int:pk>/", ArtisteDetail.as_view(), name="artiste_detail"),
-    path("", ArtisteList.as_view(), name="artiste_list"),
+    path("artiste/", ArtisteList.as_view(), name="artiste_list"),
 ] 
